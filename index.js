f54938bba1a09e5df61a8b7ac4446f792fe0349d
hayat.singh <72182653+hayatsingh-geospoc@users.noreply.github.com>
2024-01-24 23:06:13 +0530

    database connected

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const route = require('./src/routes/routes.js');
 require('dotenv').config();
 const app = express();
@@ -7,6 +8,17 @@ app.use(express.json());
 app.use('/', route);
 
 const port = process.env.PORT || 3000;
+
+mongoose
+  .connect(
+    process.env.DB_URL,
+  )
+  .then(() => {
+    console.log('MongoDB connected');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.listen(port, function () {
   console.log(`Express app running on port ${port}`);
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -4,5 +4,6 @@ const demoController = require('../controller/demo.js');
 const router = express.Router();
 
 router.get('/getdata', demoController.addition);
+router.post('/create')
 
 module.exports = router;
